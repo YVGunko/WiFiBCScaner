3fc565d565f9e328e65a8392e3194fab9f5da5cf
“YVGunko“ <yurygunko@gmail.com>
2024-04-01 14:00:02 +0300

    work in progress

--- app/src/main/java/com/example/yg/wifibcscaner/MainActivity.java
+++ app/src/main/java/com/example/yg/wifibcscaner/MainActivity.java
@@ -154,6 +154,7 @@ public class MainActivity extends AppCompatActivity implements BarcodeReader.Bar
     @Override
     public void onStop(){
         super.onStop();
+        DataSyncTimerUtil.stopDataSyncTimer();
         if(barcodeReader!=null)
             barcodeReader.release();
     }
@@ -246,6 +247,7 @@ public class MainActivity extends AppCompatActivity implements BarcodeReader.Bar
     @Override
     public void doDataSync() {
         Log.d(TAG, "doDataSync !");
+        /* what I wanted to do here */
         boxRepo.sendData();
     }
     @RequiresApi(api = Build.VERSION_CODES.N)
